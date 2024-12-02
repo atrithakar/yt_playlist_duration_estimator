7067711559d5b59507a0be9db2c769acc7105b23
Atri Thakar <atrithaker122@gmail.com>
2024-12-02 14:55:37 +0530

    upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from datetime import timedelta
+from get_durations import get_video_duration, get_playlist_duration, calculate_pomodoro_sessions
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        url = request.form["url"]
+        speed = float(request.form["speed"])
+        si = ei = original_duration = None
+        if "playlist?list" in url:
+            si = int(request.form["start_index"])
+            ei = int(request.form["end_index"])
+            formatted_duration, total_duration, id, title, original_duration, flg = get_playlist_duration(url, speed, si, ei)
+            if flg:
+                si = 1
+        elif "watch?v" in url or "youtu.be/" in url:
+            formatted_duration, total_duration, id, title, original_duration = get_video_duration(url, speed)
+        else:
+            formatted_duration = "Invalid URL"
+            total_duration = None
+            duration = None
+        
+        if total_duration is not None:
+            pomodoro_sessions, pomodoro_time = calculate_pomodoro_sessions(total_duration)
+            total_pomodoro_time = pomodoro_time + (int(pomodoro_sessions)//4)*3600
+            pomodoro_time = str(timedelta(seconds=int(pomodoro_time)))
+            total_pomodoro_time = str(timedelta(seconds=int(total_pomodoro_time)))
+        else:
+            pomodoro_sessions = pomodoro_time = total_pomodoro_time = id = title = None
+        
+        print(si)
+
+        return render_template("index.html", duration=formatted_duration, pomodoro_sessions=pomodoro_sessions, pomodoro_time=pomodoro_time, total_pomodoro_time=total_pomodoro_time,id=id,title=title,start_index=si,end_index=ei, original_duration=original_duration)
+
+    return render_template("index.html", duration=None, pomodoro_sessions=None, pomodoro_time=None)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
---
+++ get_durations.py
@@ -0,0 +1,77 @@
+import yt_dlp
+from datetime import timedelta
+
+def get_video_duration(url, speed=1):
+    try:
+        ydl_opts = {
+            'quiet': True,
+            'extract_flat': True,
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            # print(info_dict)
+            duration = info_dict.get('duration', None)
+            original_duration = None
+            id = info_dict.get('id', None)
+            title = info_dict.get('title', None)
+            if duration is not None:
+                original_duration = str(timedelta(seconds=int(duration)))
+                duration = duration / speed
+                formatted_duration = str(timedelta(seconds=int(duration)))
+                return formatted_duration, duration, id, title, original_duration
+            else:
+                return "Duration not available.", None, None, None, None
+    except Exception as e:
+        return f"Invalid Video Link", None, None, None, None
+
+
+def get_playlist_duration(playlist_url, speed=1, si=1, ei=5000):
+    try:
+        ydl_opts = {
+            'quiet': True,
+            'extract_flat': True,
+        }
+        si_gt_tot_vids_flg = False
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            result = ydl.extract_info(playlist_url, download=False)
+            # print(result)
+            id = result.get('id', None)
+            total_vids = result.get('playlist_count', None)
+            # print(total_vids, si, ei)
+            if(total_vids < si):
+                si = 1
+                si_gt_tot_vids_flg = True
+            # print(total_vids, si, ei)
+            title = result.get('title', None)
+            total_duration = 0
+            entries = result['entries'][si - 1:ei]
+            for video in entries:
+                if 'duration' in video:
+                    total_duration += video['duration']
+            original_duration = str(timedelta(seconds=int(total_duration)))
+            total_duration = total_duration / speed
+            formatted_duration = str(timedelta(seconds=int(total_duration)))
+            return formatted_duration, total_duration, id, title, original_duration, si_gt_tot_vids_flg
+    except Exception as e:
+        return f"Invalid Playlist Link", None, None, None, None, None
+
+
+def calculate_pomodoro_sessions(duration_seconds):
+    study_time = 25 * 60  # 25 minutes of study
+    break_time = 5 * 60   # 5 minutes of break
+    total_time_per_pomodoro = study_time + break_time
+
+    full_sessions = duration_seconds // study_time
+    remainder = duration_seconds % study_time
+
+    if duration_seconds <= study_time:
+        return 1, duration_seconds
+
+    if remainder > 0:
+        full_sessions += 1
+    
+    total_pomodoro_time = full_sessions * total_time_per_pomodoro
+    
+    return full_sessions, total_pomodoro_time
+
+
