91db3a1bd23b03e6d2d35d0f216a40e00f73c53e
Atri Thakar <atrithaker122@gmail.com>
2024-12-06 17:07:55 +0530

    upload 18

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from datetime import timedelta
 from get_durations import get_video_duration, get_playlist_duration, calculate_pomodoro_sessions
-import re
 
 app = Flask(__name__)
 CORS(app)
@@ -26,7 +25,6 @@ def index():
         else:
             formatted_duration = "Invalid URL"
             total_duration = None
-            duration = None
         
         if total_duration is not None:
             pomodoro_sessions, pomodoro_time = calculate_pomodoro_sessions(total_duration)
@@ -36,9 +34,19 @@ def index():
         else:
             pomodoro_sessions = pomodoro_time = total_pomodoro_time = id = title = None
 
-        return render_template("index.html", duration=formatted_duration, pomodoro_sessions=pomodoro_sessions, pomodoro_time=pomodoro_time, total_pomodoro_time=total_pomodoro_time, id=id, title=title, start_index=si, end_index=ei, original_duration=original_duration)
+        return render_template("index.html", duration=formatted_duration,
+                               pomodoro_sessions=pomodoro_sessions,
+                               pomodoro_time=pomodoro_time,
+                               total_pomodoro_time=total_pomodoro_time,
+                               id=id,
+                               title=title,
+                               start_index=si,
+                               end_index=ei,
+                               original_duration=original_duration)
 
-    return render_template("index.html", duration=None, pomodoro_sessions=None, pomodoro_time=None)
+    return render_template("index.html", duration=None,
+                           pomodoro_sessions=None,
+                           pomodoro_time=None)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000)
