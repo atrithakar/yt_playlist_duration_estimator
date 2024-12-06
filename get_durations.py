6cedcac88f14ac7e17562f021bc2921bd45eabca
Atri Thakar <atrithaker122@gmail.com>
2024-12-06 16:55:40 +0530

    upload 16

--- get_durations.py
+++ get_durations.py
@@ -1,30 +1,35 @@
 import yt_dlp
 from datetime import timedelta
+from ratelimiter import RateLimiter
+
+# Create a rate limiter that allows 10 calls per second
+rate_limiter = RateLimiter(max_calls=10, period=1)
 
 def get_video_duration(url, speed=1):
     try:
         ydl_opts = {
             'cookiefile': "cookies.txt",
             'quiet': False,
-            'verbose': True,  # Add this line to enable verbose logging
+            'verbose': True,
             'extract_flat': True,
-}
+        }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=False)
-            # print(info_dict)
-            duration = info_dict.get('duration', None)
-            original_duration = None
-            id = info_dict.get('id', None)
-            title = info_dict.get('title', None)
-            if duration is not None:
-                original_duration = str(timedelta(seconds=int(duration)))
-                duration = duration / speed
-                formatted_duration = str(timedelta(seconds=int(duration)))
-                return formatted_duration, duration, id, title, original_duration
-            else:
-                return "Duration not available.", None, None, None, None
+            with rate_limiter:  # Apply rate limiting here
+                info_dict = ydl.extract_info(url, download=False)
+                duration = info_dict.get('duration', None)
+                original_duration = None
+                id = info_dict.get('id', None)
+                title = info_dict.get('title', None)
+                if duration is not None:
+                    original_duration = str(timedelta(seconds=int(duration)))
+                    duration = duration / speed
+                    formatted_duration = str(timedelta(seconds=int(duration)))
+                    return formatted_duration, duration, id, title, original_duration
+                else:
+                    return "Duration not available.", None, None, None, None
     except Exception as e:
-        return f"Invalid Video Link", None, None, None, None
+        print(f"Error fetching video duration: {e}")
+        return "Invalid Video Link", None, None, None, None
 
 
 def get_playlist_duration(playlist_url, speed=1, si=1, ei=5000):
@@ -32,32 +37,31 @@ def get_playlist_duration(playlist_url, speed=1, si=1, ei=5000):
         ydl_opts = {
             'cookiefile': "cookies.txt",
             'quiet': False,
-            'verbose': True,  # Add this line to enable verbose logging
+            'verbose': True,
             'extract_flat': True,
-}
+        }
         si_gt_tot_vids_flg = False
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            result = ydl.extract_info(playlist_url, download=False)
-            # print(result)
-            id = result.get('id', None)
-            total_vids = result.get('playlist_count', None)
-            # print(total_vids, si, ei)
-            if(total_vids < si):
-                si = 1
-                si_gt_tot_vids_flg = True
-            # print(total_vids, si, ei)
-            title = result.get('title', None)
-            total_duration = 0
-            entries = result['entries'][si - 1:ei]
-            for video in entries:
-                if 'duration' in video:
-                    total_duration += video['duration']
-            original_duration = str(timedelta(seconds=int(total_duration)))
-            total_duration = total_duration / speed
-            formatted_duration = str(timedelta(seconds=int(total_duration)))
-            return formatted_duration, total_duration, id, title, original_duration, si_gt_tot_vids_flg
+            with rate_limiter:  # Apply rate limiting here
+                result = ydl.extract_info(playlist_url, download=False)
+                id = result.get('id', None)
+                total_vids = result.get('playlist_count', None)
+                if total_vids < si:
+                    si = 1
+                    si_gt_tot_vids_flg = True
+                title = result.get('title', None)
+                total_duration = 0
+                entries = result['entries'][si - 1:ei]
+                for video in entries:
+                    if 'duration' in video:
+                        total_duration += video['duration']
+                original_duration = str(timedelta(seconds=int(total_duration)))
+                total_duration = total_duration / speed
+                formatted_duration = str(timedelta(seconds=int(total_duration)))
+                return formatted_duration, total_duration, id, title, original_duration, si_gt_tot_vids_flg
     except Exception as e:
-        return f"Invalid Playlist Link", None, None, None, None, None
+        print(f"Error fetching playlist duration: {e}")
+        return "Invalid Playlist Link", None, None, None, None, None
 
 
 def calculate_pomodoro_sessions(duration_seconds):
@@ -77,5 +81,3 @@ def calculate_pomodoro_sessions(duration_seconds):
     total_pomodoro_time = full_sessions * total_time_per_pomodoro
     
     return full_sessions, total_pomodoro_time
-
-
